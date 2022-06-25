561ebb6c2a68eae09556bd2f645bf0c34840967e
msaifull <saifulguardian92@gmail.com>
2022-06-25 21:21:54 +0700

    sesi-16end

--- app/config/index.js
+++ app/config/index.js
@@ -1,10 +1,7 @@
 const dotenv = require("dotenv");
-
 dotenv.config();
-
 const path = require("path");
 console.log(process.env.MODE);
-
 module.exports = {
   rootPath: path.resolve(__dirname, "../../"),
   urlDb:
@@ -13,3 +10,18 @@ module.exports = {
       : process.env.MONGODB_URL_DEV,
   jwtSecret: process.env.JWT_SECRET,
 };
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+
+// const path = require("path");
+// console.log(process.env.MODE);
+
+// module.exports = {
+//   rootPath: path.resolve(__dirname, "../../"),
+//   urlDb:
+//     process.env.MODE === "PRO"
+//       ? process.env.MONGODB_URL_PRO
+//       : process.env.MONGODB_URL_DEV,
+//   jwtSecret: process.env.JWT_SECRET,
+// };
